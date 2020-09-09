2c9168ece5e7a8edf82a0e287c2fa4ca4f06a9c4
waltergrayiii <tripgray@yahoo.com>
2020-09-09 01:47:16 -0500

    intitial commit

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <time.h>
+#include <conio.h>
+using namespace std;
+enum eDir
+{
+  STOP = 0,
+  lEFT = 1,
+  UPLEFT = 2,
+  DOWNLEFT = 3,
+  RIGHT = 4,
+  UPRIGHT = 5,
+  DOWNRIGHT = 6,
+};
+class cball
+{
+private:
+  int x, y;
+  int originalX, originalY;
+  eDir direction;
+
+public:
+  cBall(int posX, int posY)
+  {
+    originalX = posX;
+    originalY = posY;
+    x = posX, y = posY;
+    direction = STOP;
+  }
+  void Reset()
+  {
+    x = originalX, y = originalY;
+    direction = STOP;
+  }
+  void changeDirection(eDir d)
+  {
+    direction = d;
+  }
+  void randomDirection()
+  {
+    direction = (eDir)((rand() % 6) + 1);
+  }
+  inline int getX() { return x; }
+  inline int getY() { return y; }
+  inline eDir getDirection() { return direction; }
+  void Move()
+  {
+    switch (direction)
+    {
+    case STOP:
+      break;
+    case LEFT:
+      x--;
+      break;
+    case RIGHT:
+      x++;
+      break;
+    case UPLEFT:
+      x--;
+      y--;
+      break;
+    case DOWNLEFT:
+      x--;
+      y++;
+      break;
+    case UPRIGHT:
+      x++;
+      y--;
+      break;
+    case DOWNRIGHT:
+      x++;
+      y++;
+      break;
+    default:
+      break;
+    }
+  }
+  friend ostream &operator<<(ostream &o, cBall c)
+  {
+    o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
+    return o;
+  }
+};
+
+int main()
+{
+
+  return 0,
+}
